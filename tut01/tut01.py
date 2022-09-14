5de84b9b5d754e71d02591e2cc543c0b7dc17604
Srujana Siripurapu <109893860+srujju123@users.noreply.github.com>
2022-09-14 23:32:12 +0530

    calculated the average and differences

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,7 +1,14 @@
 def octact_identification(mod=5000):
+    df = pandas.read_csv("octant_input.csv")
+    df.loc[0, "U Avg"] = df["U"].mean()
+    df.loc[0, "V Avg"] = df["V"].mean()
+    df.loc[0, "W Avg"] = df["W"].mean()
+    df["U'=U - U avg"] = df["U"]-df.loc[0, "U Avg"]
+    df["V'=V - V avg"] = df["V"]-df.loc[0, "V Avg"]
+    df["W'=W - W avg"] = df["W"]-df.loc[0, "W Avg"]
 ###Code
 
-
+import pandas
 from platform import python_version
 ver = python_version()
 
